b687b0ea5a87c74aa8771669c36b3d39114a7a2f
DanxzSleepy <wolfdangalaxy2@gmail.com>
2025-06-30 17:33:13 -0300

    Add new feature to user profile page
    Implemented additional functionality on the user profile page, including support for editing user details and updating the profile picture. Refactored related components for better maintainability.

--- VoidNess/urls.py
+++ VoidNess/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
+from . import views
+
+app_name = 'AvoidNess'
 
 urlpatterns = [
-    path('', include('VoidNess.urls')),
-    path('Akumaverse/',)
+    path('', views.Avoidness, name='avoidness', ),
+    path('AkumaVerse/', views.Akumaverse, name='akumaverse', ),
+    path('LumenCore/', views.LumenCore, name='lumencore', ),
+    path('NoctForge/', views.NoctForge, name='noctforge', )  
 ]
--- VoidNess/views.py
+++ VoidNess/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Avoidness(request):
+    return render(request, 'VoidNess/avoidness.html')
+
+def Akumaverse(request):
+    return render(request, 'VoidNess/akumaverse.html')  
+
+def LumenCore(request):
+    return render(request, 'VoidNess/lumencore.html')
+
+def NoctForge(request):
+    return render(request, 'VoidNess/noctforge.html')  
