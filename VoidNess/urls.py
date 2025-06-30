03713b8ba2036394af675aaec1bbebe5142acc31
DanxzSleepy <wolfdangalaxy2@gmail.com>
2025-06-30 17:55:59 -0300

    Update URLs and templates to use Django URL tags
    Replaced hardcoded links in HTML templates with Django {% url %} template tags for better maintainability and routing. Updated the root URL pattern to 'AvoidNess/' and changed its name to 'home' in urls.py to align with template references.

--- VoidNess/urls.py
+++ VoidNess/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'AvoidNess'
 
 urlpatterns = [
-    path('', views.Avoidness, name='avoidness', ),
+    path('AvoidNess/', views.Avoidness, name='home', ),
     path('Akumaverse/', views.Akumaverse, name='akumaverse', ),
     path('Lumencore/', views.LumenCore, name='lumencore', ),
     path('Noctforge/', views.NoctForge, name='noctforge', )  
